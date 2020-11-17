c080a542c3aebbc7ec4557d3b94e05283e4e6b0e
cep104 <candicelizabethpeters@gmail.com>
2020-11-17 17:07:51 -0600

    commit tree 4ca350360c9135b2a99de8f5e0b6dea68eb5066d

--- index.js
+++ index.js
@@ -8,6 +8,38 @@ function handleClick(e) {
 }
 
 /* End Given Code, don't edit above here...*/
+function greet(string){
+let num = parseInt(string)
+console.log(num) 
+// check the read me and see what string is passing through 
+
+if(num < 12 ){
+  console.log("Good Morning!")
+  return "Good Morning"
+  
+}else if (num > 17){ 
+  // make sure it is in military time ("17:00")
+  // parseInt cuts it off at :
+  return "Good Evening"
+}else{
+  return "Good Afternoon"
+}
+
+
+}
+
+function displayMessage(string){
+  let greeting = document.getElementById("greeting")
+  console.log(greeting)
+    greeting.innerText = string
+  // has to be innerTEXT look at tests in test folder for what they are trying to pass through
+}
+
+
+function testing(){
+  let message = document.getElementById("time").value
+}
+
 
 /* Write your implementation of greet() */
 /* Write your implementation of displayMessage() */
